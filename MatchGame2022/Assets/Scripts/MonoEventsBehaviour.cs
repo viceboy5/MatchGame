bf4263aed59a2ad025b2b01c470e9aa13859e326
Hank Weiss <10811528@my.uvu.edu>
2022-09-07 18:02:47 -0600

    Added events to test game

---
+++ MatchGame2022/Assets/Scripts/MonoEventsBehaviour.cs
@@ -0,0 +1,23 @@
+//Created with Anthony Romrell 2022 Scripting Class
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent startEvent, awakeEvent, disableEvent;
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        disableEvent.Invoke();
+    }
+}
