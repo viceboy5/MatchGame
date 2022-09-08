3f443ab2d6f0d66654acd34b01e4286f7d1a611e
Hank Weiss <10811528@my.uvu.edu>
2022-09-07 18:02:01 -0600

    Updated FloatData

--- MatchGame2022/Assets/Scripts/FloatData.cs
+++ MatchGame2022/Assets/Scripts/FloatData.cs
@@ -5,6 +5,10 @@ public class FloatData : ScriptableObject
 {
     public float value;
 
+    public void SetValue(float num)
+    {
+        value = num;
+    }
     public void UpdateValue(float num)
     {
         value += num;
