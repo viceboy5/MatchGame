8c4e87fbaede077d5fca3aa874c0ebb2b1cb1646
Hank Weiss <10811528@my.uvu.edu>
2022-09-13 17:33:28 -0600

    Reorganized Files

--- MatchGame2022/Assets/Scripts/IntData.cs
+++ MatchGame2022/Assets/Scripts/IntData.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
@@ -7,20 +8,31 @@ using UnityEngine.Events;
 public class IntData : ScriptableObject
 {
     public int value;
-
+    
     public void SetValue(int num)
     {
         value = num;
     }
 
+    public void CompareValue(IntData obj)
+    {
+        if (value >= obj.value)
+        {
+            
+        }
+        else
+        {
+            value = obj.value;
+        }
+    }
+
+    public void SetValue(IntData obj)
+    {
+        value = obj.value;
+    }
+
     public void UpdateValue(int number)
     {
         value += number;
-    }
-
-    public void DisplayNumber(Text txt)
-    {
-        txt.text = value.ToString();
-        Debug.Log("Value has been updated: " + value);
     }
 }
---
+++ MatchGame2022/Assets/Scripts/TransformBehaviour.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class TransformBehaviour : MonoBehaviour
+{
+    public void ResetToZero()
+    {
+        transform.position = Vector3.zero;
+    }
+}
