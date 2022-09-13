4255f76971462ffbdfd6fd8e3e9f24fcabd14f8e
Hank Weiss <10811528@my.uvu.edu>
2022-09-13 16:57:15 -0600

    Setup Score UI

--- MatchGame2022/Assets/Scripts/TextLabelBehaviour.cs
+++ MatchGame2022/Assets/Scripts/TextLabelBehaviour.cs
@@ -1,22 +1,28 @@
 using System.Globalization;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 
 [RequireComponent(typeof(Text))]
 public class TextLabelBehaviour : MonoBehaviour
 {
-    public Text label;
-    public FloatData dataObj;
+    private Text label;
+    public UnityEvent startEvent;
 
     private void Start()
     {
         label = GetComponent<Text>();
-        UpdateLabel();
+        startEvent.Invoke();
     }
 
-    public void UpdateLabel()
+    public void UpdateLabel(FloatData obj)
     {
-        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public void UpdateLabel(IntData obj)
+    {
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
     }
 }
