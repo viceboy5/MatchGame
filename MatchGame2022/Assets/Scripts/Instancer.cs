7353826d975f8ea5d6e19579133f32c7f29704e9
Hank Weiss <10811528@my.uvu.edu>
2022-09-20 23:42:24 -0600

    Setup Instancer to choose from list and setup prefab with color

--- MatchGame2022/Assets/Scripts/Instancer.cs
+++ MatchGame2022/Assets/Scripts/Instancer.cs
@@ -32,4 +32,10 @@ public class Instancer : ScriptableObject
             num = 0;
         }
     }
+    
+    public void CreateInstanceListRandomly(Vector3DataList obj)
+    {
+        num = Random.Range(0, obj.vector3List.Count);
+        Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+    }
 }
