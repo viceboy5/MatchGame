976ed17959704e6e4f921e0e705f5a57df715456
Hank Weiss <10811528@my.uvu.edu>
2022-09-20 23:26:18 -0600

    Setup V3 data OBJs and Scripts

--- MatchGame2022/Assets/Scripts/Vector3Data.cs
+++ MatchGame2022/Assets/Scripts/Vector3Data.cs
@@ -1,12 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class Vector3Data : ScriptableObject
 {
-    public int direction;
-
-    public void UpdateValue(int newDirection)
-    {
-        direction = newDirection +=1;
-    }
+    public Vector3 value;
 }
