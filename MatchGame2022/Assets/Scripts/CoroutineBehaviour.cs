0f4db0ff24bac4fc399871a63e34087a7bc7b531
Hank Weiss <10811528@my.uvu.edu>
2022-09-20 20:10:44 -0600

    Saved 321Go Scene, created CoRoutine Script

---
+++ MatchGame2022/Assets/Scripts/CoroutineBehaviour.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineBehaviour : MonoBehaviour
+{
+
+    public bool canRun = true;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+    
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        Debug.Log("Start");
+        yield return wfsObj;
+        Debug.Log("Late Start");
+
+        while (canRun)
+        {
+            yield return wfsObj;
+            Debug.Log("Run on Start");
+        }
+    }
+}
