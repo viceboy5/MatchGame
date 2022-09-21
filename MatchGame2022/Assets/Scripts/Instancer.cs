a0f000e53d629710805c4179b99f853463567b4b
Hank Weiss <10811528@my.uvu.edu>
2022-09-20 23:26:49 -0600

    Adjusted Instancer and Ring frefab

--- MatchGame2022/Assets/Scripts/Instancer.cs
+++ MatchGame2022/Assets/Scripts/Instancer.cs
@@ -3,8 +3,33 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Instancer : ScriptableObject
 {
-    public void CreateInstance(GameObject obj)
+    public GameObject prefab;
+    private int num;
+    public void CreateInstance()
     {
-        Instantiate(obj);
+        Instantiate(prefab);
+    }
+
+    public void CreateInstance(Vector3Data obj)
+    {
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
+    public void CreateInstanceFromList(Vector3DataList obj)
+    {
+        foreach (var t in obj.vector3List)
+        {
+            Instantiate(prefab, t.value, Quaternion.identity);
+        }
+    }
+    
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+    {
+        Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+        num++;
+        if (num == obj.vector3List.Count)
+        {
+            num = 0;
+        }
     }
 }
